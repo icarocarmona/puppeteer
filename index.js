651c2c2ad1d5a6b6f2296c3ef260f04025cdb460
Icaro Carmona <desenvolvimentoequipe@outlook.com.br>
2019-02-26 09:14:34 -0300

    refatorado codigo

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 const puppeteer = require('puppeteer');
 const screenshot = 'lendastv.png';
+const URL_LEGENDASTV = 'http://legendas.tv/login';
 
 (async () => {
 
     try {
-        const browser = await puppeteer.launch({ headless: true })
+
+        const browser = await puppeteer.launch({ headless: false })
         const page = await browser.newPage()
         await page.setViewport({ width: 1280, height: 800 })
 
         const navigationPromise = page.waitForNavigation()
 
-        await page.goto('http://legendas.tv/login')
-        //await page.click('body > div.container > header > section > div.login > a.js_entrar')
+        await page.goto(URL_LEGENDASTV)
 
-        await page.type('#UserUsername', 'fliperapp')
-        await page.type('#UserPassword', '123456')
+        await page.type('#UserUsername', process.env.LEGENDASTV_USER)
+        await page.type('#UserPassword', process.env.LEGENDASTV_PWS)
         await page.click('#UserLoginForm > button')
-        //await page.waitForNavigation()
 
         await navigationPromise
 
@@ -25,37 +25,33 @@ const screenshot = 'lendastv.png';
 
         await page.type('#search-box', 'The Simpsons')
         await page.keyboard.press(String.fromCharCode(13))
-        await page.waitForSelector('#resultado_busca')
+        await page.waitForSelector('#resultado_busca div')
 
 
-        await page.screenshot({ path: screenshot })
-        const res = await page.$$('#resultado_busca div')
-        console.log(res.length)
+        const legendas = await page.$$('#resultado_busca div div')
         const data = []
 
-        for (const teste of res) {
-            const nome = await teste.$eval('p', p => p.innerText)
-            //console.log(nome)
+        for (const legenda of legendas) {
+            const imagem = await legenda.$eval('img', img => img.getAttribute('src')).catch(x => null)
+            const nome = await legenda.$eval('p', p => p.innerText)
+            //#resultado_busca > div > article:nth-child(2) > div:nth-child(12) > img
+            const link = 'http://legendas.tv' + await legenda.$eval('a', img => img.getAttribute('href')).catch(x => null)
 
-            const imagem = await teste.$eval('img', img => img.getAttribute('src')).catch(x => null)
-            //console.log(imagem)    
-
-            const link =  'http://legendas.tv' + await teste.$eval('a', img => img.getAttribute('href')).catch(x => null)
-
-
-            data.push({ nome, imagem,link })
+            data.push({ nome, imagem, link })
         }
 
-        console.log(data)
-        // const legendas = await page.$$eval('div.f_left', x => {
-        //     return x.map(x => x.textContent.trim())
-        //   })
-        // console.log(legendas)
+        //Armazena no banco
+        salvadados(data)
 
+        await page.screenshot({ path: screenshot })
         browser.close()
-        //console.log('See screenshot: ' + screenshot)
 
     } catch (error) {
+        //Notifica erro ao monitoramento
         console.log(error)
     }
 })()
+
+function salvadados(data) {
+    console.log(data)
+}
